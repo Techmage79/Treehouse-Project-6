8dcde5c109d98658846d42ab554b32fd7d472c1a
Techmage79 <techmage79@gmail.com>
2021-02-09 21:55:27 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//Global Variables
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const btnReset = document.getElementsByClassName('btn__reset')[0];
+const missed = 0;
+
+const phrases = [
+    'The Lord of the Rings',
+    'The Never Ending Story',
+    'Labrynth',
+    'The Dark Crystal',
+    'Beast Master',
+    'Flight of the Navigator',
+    'Splash'
+];
+
+
+
+//Hide overlay on start screen
+btnReset.addEventListener('click', () => {
+  const overlay = document.getElementById('overlay');
+  overlay.style.display = 'none';
+});
+
+//create a getRandomPhrasesAsArray function
+function getRandomPhrase(arr) {
+const randomNumber = Math.floor(Math.random() * arr.length);
+const randomPhrase = phrases[randomNumber]
+return randomPhrase;
+}
+
+getRandomPhrase(phrases);
