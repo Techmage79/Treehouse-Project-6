158bdfb07e0cdbe652251da8a95a92a005fa7e0d
Techmage79 <techmage79@gmail.com>
2021-02-11 12:09:27 -0500

    checkLettet function

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const btnReset = document.getElementsByClassName('btn__reset')[0];
 const missed = 0;
 
 const phrases = [
-    'The Lord of the Rings',
-    'The Never Ending Story',
-    'Labrynth',
-    'The Dark Crystal',
-    'Beast Master',
-    'Flight of the Navigator',
-    'Splash'
+  'Lord of the Rings',
+  'The Never Ending Story',
+  'Labrynth',
+  'The Dark Crystal',
+  'Beast Master',
+  'Flight of the Navigator',
+  'Splash'
 ];
 
 
@@ -24,9 +24,39 @@ btnReset.addEventListener('click', () => {
 
 //create a getRandomPhrasesAsArray function
 function getRandomPhrase(arr) {
-const randomNumber = Math.floor(Math.random() * arr.length);
-const randomPhrase = phrases[randomNumber]
-return randomPhrase;
+  const randomNumber = Math.floor(Math.random() * arr.length);
+  const randomPhrase = phrases[randomNumber]
+  return randomPhrase;
 }
 
 getRandomPhrase(phrases);
+
+//Set the game display
+function addPhraseToDisplay(arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    let li = document.createElement('li');
+    let ul = document.getElementById('phrase');
+    li.textContent = arr[i]
+    ul.appendChild(li);
+    if (li.textContent === ' ') {
+      li.className = 'space';
+    } else {
+      li.className = 'letter';
+    }
+  }
+}
+const phraseArray = getRandomPhrase(phrases);
+addPhraseToDisplay(phraseArray);
+
+//Create a checkletter function
+function checkLetter(btn) {
+  const keyBoardLetters = document.querySelectorAll('li');
+  let match = null;
+  for (let i = 0; i < keyBoardLetters.length; i ++){
+    if (keyBoardLetters[i] === btn.textContent) {
+      li.className.add = 'show';
+      let match = btn.textContent;
+    }
+  }
+    return match;
+}
